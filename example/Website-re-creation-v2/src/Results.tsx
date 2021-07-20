c6b77509e513eac89ac39e98f432e25cb5f098ac
unknown <kdu2030@gmail.com>
2021-07-20 13:36:24 -0400

    Add New Tab When Button Clicked

--- example/Website-re-creation-v2/src/Results.tsx
+++ example/Website-re-creation-v2/src/Results.tsx
@@ -1,85 +1,28 @@
-import FlexLayout, { DockLocation, IJsonModel, TabNode , Model} from "flexlayout-react";
-import { useState } from "react";
+import FlexLayout, { TabNode , Model} from "flexlayout-react";
 import 'flexlayout-react/style/light.css';
 import Table from './Table';
-import Options from './Options';
+import Instructions from './Instructions';
+import {APIData} from './Home';
 
-export type APIData = {
-  [index: number]: string;
-  x: number;
-  u: number;
-  d: number;
-  xu: number;
-  xd: number;
-}
+
 
 type ResultsProps = {
-  data: Array<APIData> | null;
+  data: Array<APIData>;
   config: Model;
 }
 
 
-//Parameters: {data}: ResultsProps
-//{data, config}: ResultsProps
-
 function Results({data, config}: ResultsProps) {
-  /*let json: IJsonModel = {
-    global: {
-      tabEnableClose: true,
-      tabEnableRename: true,
-    },
-    borders: [],
-    layout: {
-      type: "row",
-      id: "#1",
-      weight: 100,
-      children: [
-        {
-          type: "tabset",
-          id: "#2",
-          weight: 100,
-          selected: 0,
-          children: [
-            {
-              type: "tab",
-              name: "Results",
-              component: "grid",
-            },
-            {
-              type: "tab",
-              name: "Options",
-              component: "options",
-            },
-          ],
-        },
-      ],
-    },
-  };
-
-
-  const [config, setConfig] = useState(FlexLayout.Model.fromJson(json));
-
-  function handleButtonClick() {
-    let newNodeJson = {
-      type: "tab",
-      name: "Results 2",
-      component: "grid",
-    };
-
-
-    config.doAction(FlexLayout.Actions.addNode(newNodeJson, "#1", DockLocation.RIGHT, 2));
-
-  }*/
-
-
 
   function factory(node: TabNode) {
     let component = node.getComponent();
-    if (component === "grid" && data == null) {
-      return <p>{node.getId()}</p>;
+    if (component === "Instructions") {
+      return <Instructions />;
     }
-    if(component === "options"){
-      return <p>Options</p>
+    else{
+      let name = node.getName();
+      let i = parseInt(name.charAt(name.length - 1));
+      return <Table data={data[i]} />
     }
   }
 
--- example/Website-re-creation-v2/src/Table.tsx
+++ example/Website-re-creation-v2/src/Table.tsx
@@ -1,8 +1,8 @@
-import { APIData } from './Results';
+import { DataPoint } from './Home';
 import 'flexlayout-react/style/light.css';
 
 type TableProps = {
-    data: Array<APIData>;
+    data: Array<DataPoint>;
 }
 
 
@@ -12,7 +12,7 @@ function Table({ data }: TableProps) {
             <table className="simple_table">
                 <tbody>
                     <tr><th>x</th><th>u</th><th>d</th><th>xu</th><th>xd</th></tr>
-                    {data.map((point: APIData) => (
+                    {data.map((point: DataPoint) => (
                         <tr><td>{point.x}</td><td>{point.u}</td><td>{point.d}</td><td>{point.xu}</td><td>{point.xd}</td></tr>
                     ))}
                 </tbody>
